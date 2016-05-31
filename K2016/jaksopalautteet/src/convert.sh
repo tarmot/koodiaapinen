588b62f626088d3f876f0957356ece9e3efad5c2
Tarmo Toikkanen <tarmo@iki.fi>
2016-05-31 23:12:46 +0300

    Combined multiline open answers to single lines for increased consistency.

--- K2016/jaksopalautteet/src/convert.sh
+++ K2016/jaksopalautteet/src/convert.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Sijoita tähän kansioon A+:n tuottamat csv-tiedostot ja aja tämä skripti.
-# Muutokset: sarakeotsikot järkeviksi, virhevastausten poisto, monivalintavastaukset numeroiksi, anonymisointi
+# Muutokset: sarakeotsikot järkeviksi, virhevastausten poisto, monivalintavastaukset numeroiksi, anonymisointi, moniriviset vastaukset yksirivisiksi
 
 for i in *.csv
 do
 	echo $i
 	echo "Time,ajankaytto,tyo_materiaali,tyo_harjoitukset,haaste_materiaali,haaste_harjoitukset,hyodyllisyys,innostavuus,soveltuvuus,mita_opin,vapaa_palaute" >../$i
-	cat $i|tail -n +2|grep ',ready,[01],'|sed 's/"option_\([0-6]\)"/\1/g'|cut -d ',' -f 1,5->>../$i
+	cat $i|tail -n +2|sed -e :a -e '/[^\"]$/N; s/\n/ /; ta'|grep ',ready,[01],'|sed 's/"option_\([0-6]\)"/\1/g'|cut -d ',' -f 1,5->>../$i
 done
