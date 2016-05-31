518b65a638025925615f10750d008c67c927bea7
Tarmo Toikkanen <tarmo@iki.fi>
2016-05-31 23:25:25 +0300

    Laskettu linjoittain ja viikoittain keskiarvot. Jaksopalautteiden 1-5 -asteikot on koodattu arvoina 0-4, joten niihin lisätään +1 keskiarvoa laskettaessa. Numerosarakkeista 4 ensimmäisen tavoite on 3 asteikolla 1-5, kolmen viimeisen tavoite on 5 asteikolla 1-5.

---
+++ K2016/jaksopalautteet/summarize.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Laskee numeeristen palautteiden keskiarvot. Koska numerot 1-5 on koodattu arvoiksi 0-4, lasketaan +1 jokaiseen arvoon.
+
+echo "jakso,tyo_materiaali,tyo_harjoitukset,haaste_materiaali,haaste_harjoitukset,hyodyllisyys,innostavuus,soveltuvuus"
+for i in *-?.csv
+do
+  printf $i
+  printf ","
+  for j in {3,4,5,6,7,8,9}
+  do
+    cat $i |cut -d ',' -f $j|awk '{ rows+=1; sum+= $1+1; } END {printf sum/rows;}'
+    printf ","
+  done
+  echo
+done
